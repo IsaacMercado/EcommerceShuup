977055df0cc48fec7acde36b5c2c151ad9a59406
Isaac Mercado <mercadoluis095@gmail.com>
2021-07-22 14:51:09 -0400

    Commit initial

---
+++ ecommerce_shuup/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testecomerce project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testecomerce.settings')
+
+application = get_wsgi_application()
